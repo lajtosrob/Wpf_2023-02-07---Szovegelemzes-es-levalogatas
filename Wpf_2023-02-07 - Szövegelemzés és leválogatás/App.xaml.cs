0e4e867b68caa08bf1ff4512330bb1cd02131cc0
lajtosrob <lajtosrobert@freemail.hu>
2023-02-08 15:29:49 +0100

    Add project files.

---
+++ Wpf_2023-02-07 - Szövegelemzés és leválogatás/App.xaml.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace Wpf_2023_02_07___Szövegelemzés_és_leválogatás
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+}
---
+++ Wpf_2023-02-07 - Szövegelemzés és leválogatás/MainWindow.xaml.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Wpf_2023_02_07___Szövegelemzés_és_leválogatás
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        string[] janosVitezSorok;
+        public MainWindow()
+        {
+            InitializeComponent();
+            
+        }
+
+        private void btnBetolt_Click(object sender, RoutedEventArgs e)
+        {
+            janosVitezSorok = File.ReadAllLines("JánosVitéz.txt");
+
+
+            for (int sorokIndex = 0; sorokIndex < janosVitezSorok.Length; sorokIndex++)
+            {
+                lbVerssorok.Items.Add(janosVitezSorok[sorokIndex]);
+            }
+        }
+
+        private void btnLevalogat_Click(object sender, RoutedEventArgs e)
+        {
+            foreach (var item in lbVerssorok.Items)
+            {
+                if (Convert.ToString(item).Contains(txtKerSzovResz.Text))
+                {
+
+                    lbLevalogatott.Items.Add(item);
+
+                }
+            }
+        }
+    }
+}
